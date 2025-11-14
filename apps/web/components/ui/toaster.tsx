b07d2ceaf083b302feb4d59315a14003b988911b
Nortey Micheal <norteymicheal91@gmail.com>
2025-11-14 22:56:03 +0000

    feat: integrate react-toastify for improved toast notifications

--- apps/web/components/ui/toaster.tsx
+++ apps/web/components/ui/toaster.tsx
@@ -1,35 +1,16 @@
-'use client'
+import { ToastContainer, toast } from 'react-toastify';
 
-import { useToast } from '@/hooks/use-toast'
-import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from '@/components/ui/toast'
-
-export function Toaster() {
-  const { toasts } = useToast()
+const Toaster = () => {
+  const notify = () => {
+    toast('This is a toast message!');
+  };
 
   return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  )
-}
+    <div>
+      <button onClick={notify}>Show Toast</button>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Toaster;
